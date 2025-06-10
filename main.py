ce4a06ea08462317f0ebbd8085a97669eb5763ce
NAZzarii <122733889+NAZzarii@users.noreply.github.com>
2025-06-10 22:01:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+print("To be\non not\nto be")
+print("\"Life is what happens\n\twhen\n\tyou're busy making other plans\"\n\tJohn Lennon")
+print("\"The best way to predict the future\n\tis to\n\t\tinvent it.\"\n\t\t\tAlan Kay")
